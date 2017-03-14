b6f358098bf2b88fbc5e8f8afc7bfd6deb20ebb4
chendida <495518592@qq.com>
2017-03-14 20:14:57 +0800

    修改闪屏

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
@@ -4,27 +4,71 @@ package cn.ucai.fulicenter.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
 
 public class SplashActivity extends AppCompatActivity {
-   int time = 2000;
+    int time = 3000;
+    @BindView(R.id.tvTime)
+    TextView tvTime;
+    @BindView(R.id.btnOnClick)
+    Button btnOnClick;
+    Unbinder bind;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        bind = ButterKnife.bind(this);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        new Handler().postDelayed(new Runnable() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                        SplashActivity.this.finish();
+                    }
+        }, time);
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
-                SplashActivity.this.finish();
+                Looper.prepare();
+                for (int i = 3; i > 0 ;i --) {
+                    handler.sendEmptyMessage(i);
+                }
+                Looper.loop();
             }
-        },time);
+        }).start();
+    }
+
+
+
+
+    @OnClick(R.id.btnOnClick)
+    public void onClick() {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        SplashActivity.this.finish();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind != null){
+            bind.unbind();
+        }
     }
 }
