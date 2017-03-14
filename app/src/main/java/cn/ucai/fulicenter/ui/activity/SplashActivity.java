1ffe16c9200d820e038ad93e48044a1ecf6308b3
chendida <495518592@qq.com>
2017-03-14 21:35:47 +0800

    修改闪屏

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/SplashActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
@@ -30,34 +31,38 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         bind = ButterKnife.bind(this);
-
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        final Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                        SplashActivity.this.finish();
-                    }
-        }, time);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Looper.prepare();
-                for (int i = 3; i > 0 ;i --) {
-                    handler.sendEmptyMessage(i);
+    protected void onResume() {
+        super.onResume();
+        if (btnOnClick.isEnabled()) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tvTime.setText(3 + "");
                 }
-                Looper.loop();
-            }
-        }).start();
+            }, 1000);
+        }
+        if (btnOnClick.isEnabled()) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tvTime.setText(2 + "");
+                }
+            }, 2000);
+        }
+        if (btnOnClick.isEnabled()) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tvTime.setText(1 + "");
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    SplashActivity.this.finish();
+                }
+            }, 3000);
+        }
     }
-
-
-
 
     @OnClick(R.id.btnOnClick)
     public void onClick() {
