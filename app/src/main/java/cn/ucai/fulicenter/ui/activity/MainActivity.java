738cbfcaa8104d96559c504449c8b182f0e0bb77
chendida <495518592@qq.com>
2017-03-14 18:33:32 +0800

    使用ButterKnife和bind解绑

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -1,15 +1,42 @@
 package cn.ucai.fulicenter.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.ucai.fulicenter.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.rbNewGoods)
+    RadioButton rbNewGoods;
+    @BindView(R.id.rbBoutique)
+    RadioButton rbBoutique;
+    @BindView(R.id.rbCategory)
+    RadioButton rbCategory;
+    @BindView(R.id.rbCart)
+    RadioButton rbCart;
+    @BindView(R.id.rbPerson_Center)
+    RadioButton rbPersonCenter;
+    @BindView(R.id.fragment)
+    FrameLayout fragment;
+    Unbinder bind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bind = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (bind != null){
+            bind.unbind();
+        }
     }
 }
