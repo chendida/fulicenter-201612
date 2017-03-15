acdf4248f248705aa50069a4062d9f95eb376a78
chendida <495518592@qq.com>
2017-03-15 10:26:32 +0800

    创建获取新品首页和精选二级页面的接口

---
+++ app/src/main/java/cn/ucai/fulicenter/model/net/INewGoodsModel.java
@@ -0,0 +1,19 @@
+package cn.ucai.fulicenter.model.net;
+
+import android.content.Context;
+
+import cn.ucai.fulicenter.model.bean.NewGoodsBean;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+public interface INewGoodsModel {
+    /**
+     * 获取新品首页或精选二级页面的数据
+     * @param context   上下文菜单
+     * @param pageId    分页的起始下标
+     * @param listener  OkHttpUtils.OnCompleteListener<NewGoodsBean[]>的子接口</>
+     */
+    void loadData(Context context,int pageId,OnCompleteListener<NewGoodsBean[]> listener);
+}
