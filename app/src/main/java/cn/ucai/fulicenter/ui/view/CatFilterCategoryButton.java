79ef1039b1c6fd8dac8df56a22eea7ba5028e858
chendida <495518592@qq.com>
2017-03-17 21:42:13 +0800

    分类二级界面的自定义View设置PopupWindow，弹出和显示

--- app/src/main/java/cn/ucai/fulicenter/ui/view/CatFilterCategoryButton.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/view/CatFilterCategoryButton.java
@@ -1,15 +1,75 @@
 package cn.ucai.fulicenter.ui.view;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.nfc.Tag;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.model.utils.L;
+
 
 /**
  * Created by Administrator on 2017/3/17.
  */
 
 public class CatFilterCategoryButton extends Button{
+    private static final String TAG  = "CatFilterCategoryButton";
+    boolean isExpand = false;
+    Context mContext;
+    PopupWindow mPopupWindow;
     public CatFilterCategoryButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
+        setCatFilterOnClickListener();
+    }
+
+    private void setCatFilterOnClickListener() {
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpand){
+                    L.e(TAG,"setOnClickListener" + isExpand+"1111");
+                    if (mPopupWindow != null && mPopupWindow.isShowing()){
+                        mPopupWindow.dismiss();
+                    }
+                }else {
+                    initPop();
+                }
+                showArrow();
+                L.e(TAG,"setOnClickListenerOver" + isExpand);
+            }
+        });
+    }
+
+    private void initPop() {
+        if (mPopupWindow == null){
+            mPopupWindow = new PopupWindow(mContext);
+            //设置宽度和高度
+            mPopupWindow.setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
+            mPopupWindow.setHeight(LinearLayout.LayoutParams.WRAP_CONTENT);
+            //设置背景颜色
+            mPopupWindow.setBackgroundDrawable(new ColorDrawable(0xbb000000));
+            TextView tv = new TextView(mContext);
+            tv.setTextColor(getResources().getColor(R.color.red));
+            tv.setTextSize(30);
+            tv.setText("CatFilterCategoryButton");
+            mPopupWindow.setContentView(tv);
+        }
+        mPopupWindow.showAsDropDown(this);
+    }
+
+    private void showArrow() {
+        L.e(TAG,"setOnClickListener" + isExpand);
+        Drawable end = getResources().getDrawable(isExpand? R.drawable.arrow2_up
+                :R.drawable.arrow2_down);
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,end,null);
+        isExpand = !isExpand;
     }
 }
