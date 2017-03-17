8dd6b1563400c3ddca7c2d5e72ffc53b853a2de7
chendida <495518592@qq.com>
2017-03-17 19:48:19 +0800

    点击排序后箭头方向的匹配

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/CategoryChildPageActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/CategoryChildPageActivity.java
@@ -1,8 +1,10 @@
 package cn.ucai.fulicenter.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -20,6 +22,11 @@ public class CategoryChildPageActivity extends AppCompatActivity {
     @BindView(R.id.tvTitleName)
     TextView tvTitleName;
     NewGoodsFragment mNewGoodsFragment;
+    @BindView(R.id.btn_sort_price)
+    Button btnSortPrice;
+    @BindView(R.id.btn_sort_addtime)
+    Button btnSortAddTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +46,21 @@ public class CategoryChildPageActivity extends AppCompatActivity {
 
     @OnClick({R.id.btn_sort_price, R.id.btn_sort_addtime})
     public void softList(View view) {
+        Drawable end = null;
         switch (view.getId()) {
             case R.id.btn_sort_price:
-                softBy = softPrice?I.SORT_BY_PRICE_ASC:I.SORT_BY_PRICE_DESC;
+                softBy = softPrice ? I.SORT_BY_PRICE_ASC : I.SORT_BY_PRICE_DESC;
                 softPrice = !softPrice;
+                end = getResources().getDrawable(softPrice?R.drawable.arrow_order_up
+                        :R.drawable.arrow_order_down);
+                btnSortPrice.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,end,null);
                 break;
             case R.id.btn_sort_addtime:
-                softBy = sortAddTime?I.SORT_BY_ADDTIME_ASC:I.SORT_BY_ADDTIME_DESC;
+                softBy = sortAddTime ? I.SORT_BY_ADDTIME_ASC : I.SORT_BY_ADDTIME_DESC;
                 sortAddTime = !sortAddTime;
+                end = getResources().getDrawable(sortAddTime?R.drawable.arrow_order_up
+                        :R.drawable.arrow_order_down);
+                btnSortAddTime.setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,end,null);
                 break;
         }
         mNewGoodsFragment.softBy(softBy);
